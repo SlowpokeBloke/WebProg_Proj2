a7945f7c7649216d86489dcc3bef13a57dacbff8
Slowpoke <mcoca4280@gmail.com>
2023-11-16 18:52:30 -0500

    scores.txt moved css updated w/ lifeline

--- index.php
+++ index.php
@@ -475,6 +475,7 @@ $high_scores = get_high_scores($scores_file);
                             <?php echo htmlspecialchars($answer); ?>
                         </button>
                     <?php endforeach; ?>
+<div class="lifeline">
                     <?php if (!in_array('fifty_fifty', $_SESSION['used_lifelines'])): ?>
                         <button type="submit" name="lifeline" value="fifty_fifty">Use 50:50</button>
                     <?php endif; ?>
@@ -484,6 +485,7 @@ $high_scores = get_high_scores($scores_file);
                     <?php if (!in_array('phone_a_friend', $_SESSION['used_lifelines'])): ?>
                         <button type="submit" name="lifeline" value="phone_a_friend">Phone a Friend</button>
                     <?php endif; ?>
+</div>
                 </form>
             </div>
             <?php if (isset($current_question['audience_response'])): ?>
